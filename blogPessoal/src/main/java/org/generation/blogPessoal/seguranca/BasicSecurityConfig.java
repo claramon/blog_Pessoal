1f7b484b5bad4d84881fb9ffcac9da56a4add483
Clara Montanhez <claramontanhez@gmail.com>
2021-08-12 15:21:37 -0300

    deploy heroku

--- blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -12,37 +12,39 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired //injeção de dependência
+	@Autowired // injeção de dependência
 	private UserDetailsService userDetailsService;
-	
+
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
-		auth.inMemoryAuthentication()
-        .withUser("root")
-        .password(passwordEncoder().encode("root"))
-        .authorities("ROLE_USER");
+		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
+				.authorities("ROLE_USER");
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{ //qdo o metodo for iniciado, instanciará o objeto http security  
-		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll() // autoriza o http, e libera os endpoints (caminhos dentro do controller) para que o cliente possa acessar sem a chave token, aqui para logar
-		.antMatchers("/usuarios/cadastrar").permitAll() // e aqui, para cadastrar
-		.anyRequest().authenticated()
-		.and().httpBasic() // utiliza o padrão basic para gerar o token
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //indica o tipo de sessão a ser utilizada, e cria uma política na API, de não guardar a sessão
-		.and().cors() 
-		.and().csrf().disable(); // desabilitar o csrf (padrão)
+	protected void configure(HttpSecurity http) throws Exception { // qdo o metodo for iniciado, instanciará o objeto
+																	// http security
+		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll() // autoriza o http, e libera os endpoints
+																			// (caminhos dentro do controller) para que
+																			// o cliente possa acessar sem a chave
+																			// token, aqui para logar
+				.antMatchers("/usuarios/cadastrar").permitAll() // e aqui, para cadastrar
+				.anyRequest().authenticated().and().httpBasic() // utiliza o padrão basic para gerar o token
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // indica o tipo de
+																									// sessão a ser
+																									// utilizada, e cria
+																									// uma política na
+																									// API, de não
+																									// guardar a sessão
+				.and().cors().and().csrf().disable(); // desabilitar o csrf (padrão)
 	}
-	
-	
-	
+
 }
